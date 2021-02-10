35130af324e3da077f6b5aaa7d41e951b97f6a13
Andhurt <andreshuertasgon@gmail.com>
2021-02-10 17:06:19 -0500

    Hasta Transient

---
+++ src/main/java/com/huertas/demos/student/StudentController.java
@@ -0,0 +1,29 @@
+package com.huertas.demos.student;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * Esta clase conforma la API LAYER o la capa API en la arquitectura base, es la que expondrá los servicios REST.
+ */
+@RestController
+@RequestMapping(path = "api/v1/student")
+public class StudentController {
+
+    private final StudentService studentService;
+
+    @Autowired //usado para inyección de dependencias!
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
+    @GetMapping
+    public List<Student> getStudents(){
+        return studentService.getStudents();
+    }
+}
---
+++ src/main/java/com/huertas/demos/student/StudentService.java
@@ -0,0 +1,25 @@
+package com.huertas.demos.student;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+//  @Component //usado para inyección de dependencias
+@Service // Usado para inyección de dependencias Pero RECONOCIENDO ESTO COMO SERVICIOS
+public class StudentService {
+
+    private final StudentRepository studentRepository;
+
+    @Autowired
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+    public List<Student> getStudents(){
+        return studentRepository.findAll();
+    }
+
+}
