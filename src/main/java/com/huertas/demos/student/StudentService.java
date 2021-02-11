7c864a84e136ba4b2d2fdd64aeacbeca5239506f
Andhurt <andreshuertasgon@gmail.com>
2021-02-10 20:47:14 -0500

    Deleting student

--- src/main/java/com/huertas/demos/student/StudentService.java
+++ src/main/java/com/huertas/demos/student/StudentService.java
@@ -32,4 +32,11 @@ public class StudentService {
         studentRepository.save(student);
     }
 
+    public void deleteStudent(Long studentId) {
+        boolean exist = studentRepository.existsById(studentId);
+        if (!exist){
+            throw new IllegalStateException("student with id " + studentId + " does not exist");
+        }
+        studentRepository.deleteById(studentId);
+    }
 }
